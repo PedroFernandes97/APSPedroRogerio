8396cf1f303c7ce0f4f134d45f9d876bcd85a0ee
roger <roger@LAPTOP-MTF4SM6G>
2020-09-30 13:17:35 -0300

    cria patternn tempplate altera main

--- src/main/java/com/mycompany/apsii/Main.java
+++ src/main/java/com/mycompany/apsii/Main.java
@@ -1,18 +1,21 @@
 
 package com.mycompany.apsii;
+import com.mycompany.apsii.FolhaPagamento.Pagamento;
+import com.mycompany.apsii.FolhaPagamento.RelatorioPagamentoRH;
+import com.mycompany.apsii.FolhaPagamento.RelatorioPagamentoContabilidade;
 
 
 public class Main {
-
    
     public static void main(String[] args) {
         Servidor s = new Servidor();
+        Pagamento f = new RelatorioPagamentoContabilidade();
         s.setRecebe(new Trabalhar());
         s.setAvaliado(new Normal());
         s.receber();
         s.progredir();
         s.promover();
-        
+        f.rodarFolha();
         
     }
     
