b76ea206d970dff7253f4f023835b4ed18226f79
roger <roger@LAPTOP-MTF4SM6G>
2020-11-29 13:45:59 -0200

    Alterada classes Facilitador, AcessarPortal e Servidor

--- src/main/java/com/mycompany/apsii/apsii_U/AcessarPortal.java
+++ src/main/java/com/mycompany/apsii/apsii_U/AcessarPortal.java
@@ -3,7 +3,7 @@ package com.mycompany.apsii.apsii_U;
 
 
 public class AcessarPortal {
-    public Facilitador facilitador;
+    
     public void processo(){
          System.out.println("cadastrar email");
          System.out.println("receber codigo de desbloqueio no email");
