cf8b78012228204042c355d065a7fd84d6dec929
roger <roger@LAPTOP-MTF4SM6G>
2020-11-29 13:46:51 -0200

    Alterada classes Facilitador, AcessarPortal e Servidor

--- src/main/java/com/mycompany/apsii/Main.java
+++ src/main/java/com/mycompany/apsii/Main.java
@@ -3,21 +3,23 @@ package com.mycompany.apsii;
 import com.mycompany.apsii.FolhaPagamento.Pagamento;
 import com.mycompany.apsii.FolhaPagamento.RelatorioPagamentoRH;
 import com.mycompany.apsii.FolhaPagamento.RelatorioPagamentoContabilidade;
+import com.mycompany.apsii.apsii_U.Facilitador;
 
 
 public class Main {
    
     public static void main(String[] args) {
         Servidor s = new Servidor();
-        Pagamento f = new RelatorioPagamentoContabilidade();
+        Pagamento f = new RelatorioPagamentoContabilidade();         
         f.setModoDeComunicacao(new ComunicarPorEmail());
         f.setModoDeComunicacao(new ComunicarPorSMS());
         s.setRecebe(new Trabalhar());
-        s.setAvaliado(new Normal());
+        s.setAvaliado(new Normal());        
         s.receber();
         s.progredir();
-        s.promover();
+        s.promover();       
         f.rodarFolha();
+        
         
     }
     
