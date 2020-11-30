70e972b745e089722e5038bc39e05bd26fe2e80d
PedroFernandes97 <pedrohff@unipam.edu.br>
2020-11-29 22:20:25 -0200

    Implementação da classe "Cadastro" no processo de acessar portal

--- src/main/java/com/mycompany/apsii/apsii_U/AcessarPortal.java
+++ src/main/java/com/mycompany/apsii/apsii_U/AcessarPortal.java
@@ -5,7 +5,11 @@ package com.mycompany.apsii.apsii_U;
 public class AcessarPortal {
     
     public void processo(){
-         System.out.println("cadastrar email");
+         Cadastro novoCadastro = new CadastroBuilder()
+                 .setNome("Rogerio")
+                 .setCPF("123.456.789-00")
+                 .setEndereco("Rua dos bobos")
+                 .build();                
          System.out.println("receber codigo de desbloqueio no email");
          System.out.println("desbloquear");
          System.out.println("cadatrar senha");
